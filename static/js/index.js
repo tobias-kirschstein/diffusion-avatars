81ba66f4d0c8bd91210fdad52fc50f5728142804
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2023-11-30 18:53:55 +0100

    Add Animation Matrix

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,22 @@
 
+EXPRESSION_ANIMATION = "37_from_76"
+
+function updateAnimationMatrix(point) {
+    const skip = 2;
+    const n = 20 - skip;
+
+    let top = Math.round((n / skip) * point.y.clamp(0, 1)) * skip;
+    let left = Math.round((n / skip) * point.x.clamp(0, 1)) * skip;
+    top = ("0000" + top).slice(-5);
+    left = ("0000" + left).slice(-5);
+    console.log(left, top);
+    // $('.animation-matrix-rgb > img').css('left', -left + '%');
+    // $('.animation-matrix-rgb > img').css('top', -top + '%');
+    $('.animation-matrix-rgb > img').attr("src", "./static/images/matrix_animation/" + EXPRESSION_ANIMATION + "/frame_" + left + "_" + top + ".jpg");
+    console.log($('.animation-matrix-rgb > img').attr("src"));
+}
+
+
 $(document).ready(function() {
 
     var carousels = bulmaCarousel.attach('#results-carousel', {
@@ -48,5 +66,46 @@ $(document).ready(function() {
             nextVideoElement.play();
         });
     }
+
+    const position = { x: 0, y: 0 }
+    const box = $('.animation-matrix');
+
+    Number.prototype.clamp = function(min, max) {
+        return Math.min(Math.max(this, min), max);
+    };
+
+    const cursor = $('.animation-matrix-cursor');
+    interact('.animation-matrix-cursor').draggable({
+        listeners: {
+            start (event) {
+                console.log(event.type, event.target)
+            },
+            move (event) {
+                position.x += event.dx
+                position.y += event.dy
+
+                event.target.style.transform =
+                    `translate(${position.x}px, ${position.y}px)`
+
+                let childPos = cursor.offset();
+                let parentPos = box.offset();
+                let childSize = cursor.outerWidth();
+                let point = {
+                    x: (childPos.left - parentPos.left),
+                    y: (childPos.top - parentPos.top)
+                };
+                point = {
+                    x: (point.x) / (box.innerWidth() - childSize),
+                    y: (point.y) / (box.innerHeight() - childSize)
+                }
+                updateAnimationMatrix(point);
+            },
+        },
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'parent'
+            })
+        ]
+    });
 
 })
